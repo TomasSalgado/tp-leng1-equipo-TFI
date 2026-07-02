0a28cf6643a6ab7de89654735787433d42381108
Fiamma Barrios <fiamma.barrios@unahur.edu.ar>
2026-07-01 21:13:37 -0300

    feat: Nueva funcion modo oscuro

---
+++ js.js
@@ -0,0 +1,4 @@
+//MODO OSCURO
+const modoActivo = document.body.classList.contains("modo-oscuro");
+const btnModo = document.getElementById("btn-modo");
+btnModo.addEventListener("click", () =>{document.body.classList.toggle("modo-oscuro")});
